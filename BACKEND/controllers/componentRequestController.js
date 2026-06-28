616bc199aa8a4b94364bf0d80dc5937d23440790
Neha sharma <ineffable13.n@gmail.com>
2026-06-28 16:52:48 +0530

    Integrated Component Requests backend

--- BACKEND/controllers/componentRequestController.js
+++ BACKEND/controllers/componentRequestController.js
@@ -4,10 +4,16 @@ export const fetchRequests = async (req, res) => {
   try {
     const requests = await getAllRequests();
 
-    res.status(200).json(requests);
+    res.status(200).json({
+      success: true,
+      data: requests,
+    });
   } catch (error) {
+    console.error("Component Request Error:", error);
+
     res.status(500).json({
-      message: error.message,
+      success: false,
+      message: "Failed to fetch component requests",
     });
   }
 };
--- BACKEND/services/ComponentRequestService.js
+++ BACKEND/services/ComponentRequestService.js
@@ -2,10 +2,26 @@ import { pool } from "../config/db.js";
 
 export const getAllRequests = async () => {
   const result = await pool.query(`
-    SELECT *
-    FROM component_requests
-    ORDER BY request_date DESC
-  `);
+    SELECT
+    cr.request_id,
+    cr.request_date,
+    cr.status,
+    cr.purpose,
+    c.component_name,
+    ri.quantity,
+    t.team_name,
+    s.name AS student_name,
+    s.enrollment_no
+FROM component_requests cr
+JOIN request_items ri
+    ON cr.request_id = ri.request_id
+JOIN components c
+    ON ri.component_id = c.component_id
+JOIN teams t
+    ON cr.team_id = t.team_id
+JOIN students s
+    ON t.leader_id = s.student_id
+ORDER BY cr.request_date DESC;`);
 
   return result.rows;
 };
